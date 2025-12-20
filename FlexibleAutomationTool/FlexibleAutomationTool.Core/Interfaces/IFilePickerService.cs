c72ef496c5571523bc3d7c25bce8b6027f42a87d
Oleksandr Tunik <70912192+Smegalex@users.noreply.github.com>
2025-12-20 15:27:39 +0000

    Final coursework fixes
    diagrams and whatnot

--- FlexibleAutomationTool/FlexibleAutomationTool.Core/Interfaces/IFilePickerService.cs
+++ FlexibleAutomationTool/FlexibleAutomationTool.Core/Interfaces/IFilePickerService.cs
@@ -2,7 +2,6 @@ namespace FlexibleAutomationTool.Core.Interfaces
 {
     public interface IFilePickerService
     {
-        // Returns null if user cancelled
         string? PickFile(string filter = "All files|*.*");
     }
 }
